1036d1468e7061979ca9092c4b3aec767bc91253
jmaeda <greenyoshi711@gmail.com>
2017-09-28 10:16:17 -0700

    change spacing

--- src/scan_subscriber_j.py
+++ src/scan_subscriber_j.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python
-
 import rospy
 from sensor_msgs.msg import LaserScan
 from Tkinter import *
 
 class ScanMonitor:
-	def __int__(self,scan_topic):
+	def __init__(self, scan_topic):
 		self.scan_topic_name = scan_topic
 
-
 	def scanCallback(self,msg):
-		print("Range array has " + str(len(msg, ranges)) + " elements.")
-		print("Angle increment is " + str(msg.angle_increment))
-		print(str(len(msg.ranges)*msg.angle_increment))
-	
+		#ranges contains 360 float measurements 
+		print("Range array has " + str(len(msg.ranges)) + " elements.")
+		print("Angle increement is " + str(msg.angle_increment))
+
 	def start(self):
 		root = Tk()
 		rospy.Subscriber(self.scan_topic_name,LaserScan,self.scanCallback)
-		root.mainloop()
-
-
-
+		root.mainloop() #similar to spin, but this method is more compatible with ui
 
 def main():
 	rospy.init_node('scan_monitor')
-	scan_monitor = ScanMonitor("/robot0/laser1")
+	scan_monitor = ScanMonitor("/robot0/laser_1")
 	scan_monitor.start()
-	
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
 	main()
